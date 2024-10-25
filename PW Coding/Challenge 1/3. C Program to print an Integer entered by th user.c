5b2f3697964eea73b3f1adb48812504d915a56ac
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2024-10-25 20:18:59 +0530

    C program to Print an integer entered by th user
    C program to Print an integer entered by th user

---
+++ PW Coding/Challenge 1/3. C Program to print an Integer entered by th user.c
@@ -0,0 +1,14 @@
+//  C program to Print an integer entered by th user
+
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+  int a;
+  int b;
+  printf("Enter a first number:",a);
+  scanf("%d",&a);
+  printf("Enter a Second number:",b);
+  scanf("%d",&b);
+  printf("number is: %d: and %d",a,b);
+}
