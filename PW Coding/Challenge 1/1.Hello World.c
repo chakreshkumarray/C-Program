4cfe9ce1c013059971ae49b4bd8fa6d2dd99f440
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2024-10-25 20:08:16 +0530

    Delete PW Coding/Challenge 1 directory

--- PW Coding/Challenge 1/1.Hello World.c
+++
@@ -1,7 +0,0 @@
-//   Hello World in C Programming
-
-#include<stdio.h>
-int main()
-{
-  printf("hello world");
-}
--- PW Coding/Challenge 1/2.Print Your Name.c
+++
@@ -1,7 +0,0 @@
-// print Your name in c programming 
-
-#include<stdio.h>
-int main()
-{
-  printf("Chakresh Kumar Ray");
-}
