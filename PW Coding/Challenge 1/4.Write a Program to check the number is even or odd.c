cd83279b79d26db7b8f3c436e43071af54cd93b0
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2024-10-27 20:53:34 +0530

    C Program to check the number is even or odd
    Write a C Program to check the number is even or odd

---
+++ PW Coding/Challenge 1/4.Write a Program to check the number is even or odd.c
@@ -0,0 +1,17 @@
+// Write a C Program to check the number is even or odd
+
+#include<stdio.h>
+int main()
+{
+  int n;
+  printf("enter a number:");
+  scanf("%d",&n);
+  if(n%2==0)
+  {
+  printf("chakresh kumar ray");  // printf("number is even");
+  }
+  else{
+    printf("chkaresh kumar");  // printf("number is odd");
+  }
+  return 0;
+}
