446ef0156618bd730ab806e0066f41d2318b6728
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2024-10-27 20:56:53 +0530

    C Program to Print the ASCII Value of a Character
    C Program to Print the ASCII Value of a Character

---
+++ PW Coding/Challenge 1/7.C Program to Print the ASCII Value of a Character.c
@@ -0,0 +1,10 @@
+// C Program to Print the ASCII Value of a Character
+
+#include<stdio.h>
+int main()
+{
+  char a;
+  printf("Enter a character:");
+  scanf("%c",&a);
+  printf("ASCII value of %c=%d",a);
+}
