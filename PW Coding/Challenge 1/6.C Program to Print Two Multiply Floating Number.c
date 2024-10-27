baaaffff1dd8a861ed797bdaa69aaee05872a64d
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2024-10-27 20:55:54 +0530

    C Program to Print Two Multiply Floating Number
    C Program to Print Two Multiply Floating Number

---
+++ PW Coding/Challenge 1/6.C Program to Print Two Multiply Floating Number.c
@@ -0,0 +1,16 @@
+// C Program to Print Two Multiply Floating Number
+
+#include<stdio.h>
+int main()
+{
+  float a;
+  float b;
+  float c;
+  printf("Enetr Fisrt float number:",a);
+  scanf("%f",&a);
+  printf("Enter Second float number :",b);
+  scanf("%f",&b);
+  c=a*b;
+  printf("Product of Number is :%f",c);
+
+}
