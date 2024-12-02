ea93bc06e0f2ec6a83aab55e42b62be06dd7c092
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2024-12-02 18:55:51 +0530

    Delete PRACTCE directory

--- PRACTCE/First.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-int main()
-{
-  printf("chakresh kumar ray\n");
-  printf("my age is 28");
-  return  0;
-}
--- PRACTCE/String-Initialization-format-specifier.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-int main()
-{
-  char c[50];
-  printf("enter your name : ");
-  scanf("%7s",c);
-  printf("Good Morning %s",c);
-}
