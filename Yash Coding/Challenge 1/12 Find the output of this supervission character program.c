096da1bacaed7560b29408bf886943100d720d93
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2025-01-06 11:23:41 +0530

    12 Find the output of supervission character .c
    12 Find the output of this supervission character program.c

---
+++ Yash Coding/Challenge 1/12 Find the output of this supervission character program.c
@@ -0,0 +1,9 @@
+// Find the output of this supervission character program
+#include<stdio.h>
+int main()
+{
+  int a,b,c;
+  scanf("%d %*d %d",&a,&b,&c);
+  printf("%d %d %d",a,b,c);
+  return 0;
+}
