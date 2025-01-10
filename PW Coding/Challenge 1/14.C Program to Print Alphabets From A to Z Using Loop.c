c0fb548fffaadb996e182e39c05e9d970f0fc84b
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2025-01-10 16:23:50 +0530

    Print Alphabets From A to Z Using Loop
    C Program to Print Alphabets From A to Z Using Loop

---
+++ PW Coding/Challenge 1/14.C Program to Print Alphabets From A to Z Using Loop.c
@@ -0,0 +1,12 @@
+//C Program to Print Alphabets From A to Z Using Loop
+#include<stdio.h>
+int main()
+{
+ char ch;
+ printf("Alphabets from A to Z:\n");
+ for (ch='A';ch<='Z';ch++) 
+ {
+ printf("%c",ch);
+ }
+ return 0;
+}
