9fcd1d96e06618c1bbcb2aeca0f4979a8893c4ec
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2025-01-10 16:21:07 +0530

    C Program to Print Prime Numbers From 1 to N
    C Program to Print Prime Numbers From 1 to N

---
+++ PW Coding/Challenge 1/11.C Program to Print Prime Numbers From 1 to N.c
@@ -0,0 +1,26 @@
+
+//C Program to Print Prime Numbers From 1 to N
+
+#include <stdio.h>
+int main()
+{
+ int i,j,k,n;
+ printf("Enter a number (N): ");
+ scanf("%d",&n);
+ printf("Prime numbers between 1 and %d are: ", n);
+ for(i=2; i<=n; ++i)
+ {
+ k= 1;
+ for(j=2; j<=i/2; ++j) 
+ {
+ if(i%j==0) 
+ {
+ k= 0;
+ break;
+ }
+ }
+ if (k)
+ printf("%d",i);
+ }
+ return 0;
+}
