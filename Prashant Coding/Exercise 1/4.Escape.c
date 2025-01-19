8a63865e6d5a69e4e890f2f431af2ef0f929d698
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2025-01-19 20:38:36 +0530

    Escape
    Escape

---
+++ Prashant Coding/Exercise 1/4.Escape.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int  main()
+{
+ printf("chakresh kumar ray");
+ printf("chakresh\tkumar\tray");
+ printf("chakresh\nkumar\nray");
+ printf("chakresh\bkumar\bray");
+ printf("chakresh\'kumar\'ray");
+ printf("chakresh\"kumar\"ray");
+ printf("chakresh\\kumar\\ray");
+}
