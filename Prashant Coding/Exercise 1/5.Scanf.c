5fff713de0ef8410198d4ce4c12cbd43692e126e
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2025-01-19 20:39:27 +0530

    Scanf
    Scanf

---
+++ Prashant Coding/Exercise 1/5.Scanf.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+  int a;
+  scanf("%c",&a);
+  printf("%c",a);
+
+  int age;
+  printf("Please enter your age:");
+  scanf("%d",&age);
+
+  printf("Your age is :%d",age);
+  return 0;
+}
