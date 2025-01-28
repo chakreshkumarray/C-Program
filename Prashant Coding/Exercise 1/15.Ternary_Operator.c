f94a209cbbe02da0343f2db551bd9891d3d2286d
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2025-01-28 19:06:03 +0530

    Ternary_Operator
    Ternary_Operator

---
+++ Prashant Coding/Exercise 1/15.Ternary_Operator.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+  char gender;
+  printf("Please enter your gender(M/F):");
+  scanf("%c",&gender);
+
+  gender=='M'?printf("Male"):printf("Female");
+  return 0;
+}
