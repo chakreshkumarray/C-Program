6b1ee2e3e7668cfc52a6a78a4c36a160e97e7df9
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2025-01-28 19:15:47 +0530

    Infinite_Loop
    Infinite_Loop

---
+++ Prashant Coding/Exercise 1/23.Infinite_Loop.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+  int n;
+  do{
+    printf("Enter a number: ");
+    scanf("%d",&n);
+    }while (n<=10);
+  printf("this is correct number gussed by the user");
+
+  return 0;
+}
