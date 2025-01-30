d17e67399e9e0f85e1dcc2c433ac5f224e1d42ef
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2025-01-30 17:17:48 +0530

    data-type-sizeof
    data-type-sizeof

---
+++ Prashant Coding/Challenge 1/5-data-type-sizeof.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+  int a;
+  char b;
+  float c;
+  double d;
+  printf("the size of a is %d\n ",sizeof(a));
+  printf("the size of b is %c\n ",sizeof(b));
+  printf("the size of c is %f\n ",sizeof(c));
+  printf("the size of d is %lf\n ",sizeof(d));
+
+return 0;
+}
