9a63a36592fe1aaa4162905ffe2c94abb7c567f2
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2025-01-30 14:01:11 +0530

    Pointer_Arithmetic
    Pointer_Arithmetic

---
+++ Prashant Coding/Exercise 3/53.Pointer_Arithmetic.c
@@ -0,0 +1,19 @@
+
+#include<stdio.h>
+int SumArray(int *arr[],int size)
+{
+  int Sum=0;
+  for(int i=0;i<size;i++)
+  {
+    Sum=Sum+arr[i];
+  }
+  return Sum;
+}
+
+int main(){
+  int myArray[]={1,2,3,4,5,7,8};
+  int size=sizeof(myArray)/sizeof(myArray[0]);
+  int total=SumArray(myArray,size);
+  printf("Sum of Array is :%d\n",total);
+  return 0;
+}
