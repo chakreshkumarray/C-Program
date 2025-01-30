fc9d8543ba0080f0208bdd8c1e85b67625b0d7b9
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2025-01-30 13:50:28 +0530

    Array_Initialization
    Array_Initialization

---
+++ Prashant Coding/Exercise 2/46.Array_Initialization.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+ int marks[5]={90,25,66,34,85};
+
+ printf("marks of student 1 are :%d\n",marks[0]);
+ printf("marks of student 1 are :%d\n",marks[1]);
+ printf("marks of student 1 are :%d\n",marks[2]);
+ printf("marks of student 1 are :%d\n",marks[3]);
+ printf("marks of student 1 are :%d",marks[4]);
+ return 0;
+}
