9b3b5effdca7297cf61046c2669c212b71be832b
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2025-01-30 13:42:51 +0530

    Signed_&_Unsinged
    Signed_&_Unsinged

---
+++ Prashant Coding/Exercise 2/39.Signed_&_Unsinged.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+  unsigned int length=10;
+  unsigned int width=5;
+  unsigned area=length*width;
+  printf("Area of rectangle:%u",area);
+  return 0;
+}
+
+
+
+
+
