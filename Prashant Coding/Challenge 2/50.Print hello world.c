02505cc15c93c320b239b3221b2d5707a6d8de30
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2025-02-03 11:23:37 +0530

    Function
    Function

---
+++ Prashant Coding/Challenge 2/50.Print hello world.c
@@ -0,0 +1,11 @@
+/*Write a function named greet that prints "Hello, World!" when called.*/
+#include<stdio.h>
+void greet()
+{
+ printf(" hello Chakresh kumar Ray");
+}
+int main()
+{
+  greet();
+  return 0;
+}
