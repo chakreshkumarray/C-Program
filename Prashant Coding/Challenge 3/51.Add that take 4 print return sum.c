5f15bf42ad8ad872a0fa9e28ab62ad0680f35c26
Chakresh Kumar Ray <kumarchakresh79@gmail.com>
2025-02-03 13:00:07 +0530

    51.Add that take 4 print return sum.c
    Function

---
+++ Prashant Coding/Challenge 3/51.Add that take 4 print return sum.c
@@ -0,0 +1,12 @@
+/* Write a function that adds that takes 4 int parameters and returns the
+sum.*/
+#include<stdio.h>
+int sum(int a,int b ,int c,int d)
+{
+  return a+b+c+d;
+}
+int main()
+{
+  printf("Ths sum is: %d",sum(15,17,19,18));
+}
+
